1c12497f3a875ce1a1f4566e457294efad3de1e9
Quentin <quentin.jungo@gmail.com>
2022-12-12 12:55:00 +0100

    cpp08 trying

--- cpp08/ex02/MutantStack.h
+++ cpp08/ex02/MutantStack.h
@@ -2,7 +2,6 @@
 #define MUTANTSTACK_H
 #include <iostream>
 #include <iterator>
-#include <list>
 #include <stack>
 
 template <typename T> class MutantStack : public std::stack<T> {
@@ -10,33 +9,68 @@ public:
   /* Constructors and Destructor */
   MutantStack() {}
   MutantStack(const MutantStack &other) {}
-  ~MutantStack() {}
+  virtual ~MutantStack() {}
 
   /* Operator overload */
-  MutantStack &operator=(const MutantStack &rhs) {}
+  // virtual MutantStack &operator=(const MutantStack &rhs) {}
 
   /* Element access */
-  const T &top() const {}
+  virtual const T &top() const { return _container.back(); }
 
   /* Capacity */
-  const bool empty() const {}
-  const int size() const {}
+  virtual const bool empty() const { return _container.empty(); }
+  virtual const int size() const { return _container.size(); }
 
   /* Modifiers */
-  void push(const T &new_element) {}
-  void pop() {}
-  // void swap(?) {} // C++11
-  // void emplace(?) {} // C++11
-  /* TODO : font parti de list de base... ?
-  void back() {}
-  void push_back(const T &new_element) {}
-  void pop_back() {}
-  */
+  virtual void push(const T &new_element) { _container.push_back(new_element); }
+  virtual void pop() { _container.pop_back(); }
 
-  class iterator : public std::forward_iterator_tag {};
+  /* Iterator class */
+  class iterator : public std::iterator<std::bidirectional_iterator_tag, T> {
+  private:
+    typename std::deque<T>::iterator _it;
+    std::deque<T> &_containerRef;
+    iterator() { std::cout << "default called !" << std::endl; }
+
+  public:
+    iterator(std::deque<T> &containerRef, bool is_end)
+        : _containerRef(containerRef) {
+      if (!is_end) {
+        _it = _containerRef.begin();
+      } else {
+        // TODO la valeur de deferencement de end() va automatiquement etre 0.
+        // donc s'il y a un 0 dans la list...
+        _it = _containerRef.end();
+      }
+    }
+
+    iterator &operator=(const iterator &rhs) {
+      _containerRef = rhs._containerRef;
+      _it = rhs._it;
+      return *this;
+    }
+
+    iterator &operator--() {
+      _it--;
+      return *this;
+    }
+
+    iterator &operator++() {
+      _it++;
+      return *this;
+    }
+
+    bool operator==(const iterator &other) const { return *this == other; }
+    bool operator!=(const iterator &other) const { return !(*this == other); }
+    T operator*() const { return *_it; }
+  };
+
+  /* Iterator method */
+  iterator begin() { return iterator(_container, false); }
+  iterator end() { return iterator(_container, true); }
 
 private:
-  std::list<T> _container;
+  std::deque<T> _container;
 };
 
 #endif /* MUTANTSTACK_H */
--- cpp08/ex02/main.cpp
+++ cpp08/ex02/main.cpp
@@ -1,15 +1,62 @@
-#include "Span.h"
+#include "MutantStack.h"
+#include <iostream>
+#include <vector>
+
+void other_test() {
+  std::vector<int> v;
+  v.push_back(1);
+  v.push_back(2);
+  v.push_back(3);
+
+  std::vector<int>::iterator it = v.begin();
+  for (int i = 0; i < 50; i++) {
+    std::cout << *it << std::endl;
+    it++;
+  }
+}
 
 int main(void) {
-  Span my_span(1);
-  // TODO il ne addNumber que 9 fois !
-  for (int i = 0; i < 11; i++) {
-    try {
-      my_span.addNumber(rand() % 100);
-      my_span.longestSpan();
-      std::cout << "number added !" << std::endl;
-    } catch (std::exception &e) {
-      std::cout << "oh: " << e.what() << std::endl;
-    }
+  other_test();
+  return 0;
+
+  MutantStack<int> my_stack;
+
+  my_stack.push(87);
+  my_stack.push(100);
+  my_stack.push(98);
+  std::cout << "top " << my_stack.top() << std::endl;
+  std::cout << my_stack.size() << std::endl;
+  my_stack.pop();
+  std::cout << my_stack.size() << std::endl;
+
+  // MutantStack<int>::iterator my_it = std::find(my_stack.begin(),
+  // my_stack.end(), 100);
+  MutantStack<int>::iterator my_it = my_stack.begin();
+  std::cout << "*my_it " << *my_it << std::endl;
+  my_it = my_stack.end();
+  std::cout << "*my_it " << *my_it << std::endl;
+
+  std::cout << std::endl << "ft tests: " << std::endl;
+  MutantStack<int> mstack;
+  mstack.push(5);
+  mstack.push(17);
+  std::cout << mstack.top() << std::endl;
+  mstack.pop();
+  std::cout << mstack.size() << std::endl;
+  mstack.push(3);
+  mstack.push(5);
+  mstack.push(737);
+  // mstack.push(0);
+  mstack.push(1);
+  mstack.push(2);
+  MutantStack<int>::iterator it = mstack.begin();
+  MutantStack<int>::iterator ite = mstack.end();
+  ++it;
+  --it;
+  while (it != ite) {
+    std::cout << *it << std::endl;
+    ++it;
   }
+  std::stack<int> s(mstack);
+  return 0;
 }
