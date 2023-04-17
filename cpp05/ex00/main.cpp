b1d5c74d27c6fdb1ce23f6964d9d84c27032eeee
Quentin Jungo <quentin.jungo@gmail.com>
2023-04-17 22:07:16 +0200

    5/00 clean

--- cpp05/ex00/main.cpp
+++ cpp05/ex00/main.cpp
@@ -1,17 +1,37 @@
 #include "Bureaucrat.h"
 #include <iostream>
 
-int main() {
+int main()
+{
   Bureaucrat c("Cindy", 150);
   Bureaucrat b("Bob", 1);
-  try {
+  try
+  {
     c.decrementGrade();
-  } catch (Bureaucrat::GradeTooLowException &e) {
+  }
+  catch (Bureaucrat::GradeTooLowException &e)
+  {
     std::cout << "Le grade de " << c.getName() << " est trop bas (" << e.what()
               << ")" << std::endl;
-  } catch (std::exception &e) {
+  }
+  catch (std::exception &e)
+  {
     std::cout << e.what() << std::endl;
   }
   std::cout << c << std::endl;
-  // c.decrementGrade();
+  try
+  {
+    b.incrementGrade();
+  }
+  catch (Bureaucrat::GradeTooHighException &e)
+  {
+    std::cout << "Le grade de " << b.getName() << " est trop haut (" << e.what()
+              << ")" << std::endl;
+  }
+  catch (std::exception &e)
+  {
+    std::cout << e.what() << std::endl;
+  }
+  std::cout << b << std::endl;
+  return 0;
 }
